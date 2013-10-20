422c13603f046e22c77cc99e2925b366d4155bf7
andreas <andreas.rieger@gmail.com>
2013-10-20 13:53:11 +0200

    Fix deprecated django.conf.urls.defaults import

--- munin/urls.py
+++ munin/urls.py
@@ -1,9 +1,12 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import patterns
+except ImportError:
+    from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('',
-                       (r'^total_users/$','munin.views.total_users'),
-                       (r'^active_users/$','munin.views.active_users'),
-                       (r'^total_sessions/$','munin.views.total_sessions'),
-                       (r'^active_sessions/$','munin.views.active_sessions'),
-                       (r'^db_performance/$','munin.views.db_performance'),
+                       (r'^total_users/$', 'munin.views.total_users'),
+                       (r'^active_users/$', 'munin.views.active_users'),
+                       (r'^total_sessions/$', 'munin.views.total_sessions'),
+                       (r'^active_sessions/$', 'munin.views.active_sessions'),
+                       (r'^db_performance/$', 'munin.views.db_performance'),
 )
