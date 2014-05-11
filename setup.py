854ea2cbc550218eab9f9f94a7706a81736e6913
Anders Pearson <anders@columbia.edu>
2014-05-11 18:03:01 -0400

    release 0.1.5

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-munin",
-    version="0.1.4",
+    version="0.1.5",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="http://github.com/ccnmtl/django-munin/",
