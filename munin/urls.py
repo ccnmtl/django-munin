bec989062ca125dc7622c3d9dda37c677184bd7f
Arnaud-D <35631001+Arnaud-D@users.noreply.github.com>
2023-10-12 08:55:23 +0200

    Update urls.py for Django 4 compatibility

--- munin/urls.py
+++ munin/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from munin.views import total_users, active_users, total_sessions, \
     active_sessions, db_performance
 
 urlpatterns = [
-    url(r'^total_users/$', total_users),
-    url(r'^active_users/$', active_users),
-    url(r'^total_sessions/$', total_sessions),
-    url(r'^active_sessions/$', active_sessions),
-    url(r'^db_performance/$', db_performance),
+    re_path(r'^total_users/$', total_users),
+    re_path(r'^active_users/$', active_users),
+    re_path(r'^total_sessions/$', total_sessions),
+    re_path(r'^active_sessions/$', active_sessions),
+    re_path(r'^db_performance/$', db_performance),
 ]
